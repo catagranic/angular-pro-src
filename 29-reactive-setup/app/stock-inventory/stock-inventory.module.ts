1aaecb6dd685c4ae801c48b8598a25bf910e5e1b
Catherine Granic <catherinegranic@gmail.com>
2019-01-04 15:53:19 -0300

    Reactive Forms

--- 29-reactive-setup/app/stock-inventory/stock-inventory.module.ts
+++ 29-reactive-setup/app/stock-inventory/stock-inventory.module.ts
@@ -1,19 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { StockInventoryComponent } from './containers/stock-inventory/stock-inventory.component';
 
+import { StockBranchComponent } from './components/stock-branch/stock-branch.component';
+import { StockProductsComponent } from './components/stock-products/stock-products.component';
+import { StockCounterComponent } from './components/stock-counter/stock-counter.component';
+import { StockSelectorComponent } from './components/stock-selector/stock-selector.component';
+
+import { StockInventoryService } from './services/stock-inventory.service';
+
 @NgModule({
   declarations: [
-    StockInventoryComponent
+    StockInventoryComponent,
+    StockBranchComponent,
+    StockProductsComponent,
+    StockSelectorComponent,
+    StockCounterComponent
   ],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpModule
   ],
   exports: [
     StockInventoryComponent
+  ],
+  providers: [
+    StockInventoryService
   ]
 })
 export class StockInventoryModule {}
